92de94a3090344a37a01016cc03e7fd8fff014a3
sahirbhat <sahirbhat007@gmail.com>
2025-01-20 19:00:37 +0530

    'about'

--- itfuture/accounts/views.py
+++ itfuture/accounts/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+
+
+def about(request):
+    return render(request,'accounts/about.html')
